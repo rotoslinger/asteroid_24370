cd42197956087bb3a2a0786da66557f04b186dc6
rotoslinger <harrison.levi.l@gmail.com>
2020-05-17 09:17:36 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,232 @@
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+f f f f f f f f f f f f f f f f 
+f f 5 f f f f f f f f f f f f f 
+f 5 5 5 f f f f f f f f f f f f 
+f f 5 f f f f f f f f f f f f f 
+f f f f f f f 5 f f f f f f f f 
+f f f f f f f f f f f f f 5 f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f 5 f f f f f f 
+f f 5 f f f f f f 5 f f f f f f 
+f f f f f f f 5 5 5 5 5 f f f f 
+f f f f f f f f f 5 f f f f f f 
+f f f f f f f f f 5 f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f 5 f f f f f f f f f f f 5 f 
+f f f f f f f f f f f f f f f f 
+`
+    //% blockIdentity=images._tile
+    export const tile3 = img`
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+`
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`
+    //% blockIdentity=images._tile
+    export const tile5 = img`
+7 7 7 7 7 5 7 7 7 7 7 7 5 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 5 7 7 7 7 
+7 7 7 7 7 5 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 5 7 7 7 7 7 7 7 
+7 7 5 7 7 7 7 7 7 7 7 7 7 5 7 7 
+7 7 7 7 5 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 5 7 7 5 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 5 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 5 7 7 7 5 7 7 5 7 5 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 5 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 5 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`
+    //% blockIdentity=images._tile
+    export const tile6 = img`
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+`
+}
+scene.onOverlapTile(SpriteKind.Player, myTiles.tile5, function (sprite, location) {
+    game.over(true, effects.confetti)
+})
+scene.onHitWall(SpriteKind.Player, function (sprite) {
+    info.changeLifeBy(-1)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (is_button_A_pressed) {
+        mySprite.vy = 0
+        is_button_A_pressed = false
+    } else {
+        mySprite.vy = 30
+        is_button_A_pressed = true
+    }
+})
+let is_button_A_pressed = false
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+. . . . . . . . . c c 8 . . . . 
+. . . . . . 8 c c c f 8 c c . . 
+. . . c c 8 8 f c a f f f c c . 
+. . c c c f f f c a a f f c c c 
+8 c c c f f f f c c a a c 8 c c 
+c c c b f f f 8 a c c a a a c c 
+c a a b b 8 a b c c c c c c c c 
+a f c a a b b a c c c c c f f c 
+a 8 f c a a c c a c a c f f f c 
+c a 8 a a c c c c a a f f f 8 a 
+. a c a a c f f a a b 8 f f c a 
+. . c c b a f f f a b b c c 6 c 
+. . . c b b a f f 6 6 a b 6 c . 
+. . . c c b b b 6 6 a c c c c . 
+. . . . c c a b b c c c . . . . 
+. . . . . c c c c c c . . . . . 
+`, SpriteKind.Player)
+tiles.setTilemap(tiles.createTilemap(
+            hex`100032000202020206060606060606060606020202020202060606060606060606020202020202020206060606060606060202020202020202060606060606060202020202020202020206060606060202020202020202020202060606020202020202020202020202020206060202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202030303020202020202020202020202020304030202020202020202020202020203040302020202020202020202020202030303020202020202020202020202020202020202020202020202020202020202020202030304030303020202020202020202020304040403020202020202020202020203030303030202020202020202020202020202020202020202020202020202020202020202020202020202020202030303030303030202020202020203030304030303030303030202020203030303040304040303030303020202030303030303030403030303030202030303030303030304030303030303020303030303030303030304040403030204040303040403030303040404030302040403030404030303030404040303020404030303030303030304040403030203030303030403030303040404030302030303030304030303030404040303020203030404040303030404040403020202030304040403030303040404030202020203030304040303030303030202020202020203030303030303020202020205050505050505050505050505050505`,
+            img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . 2 . . . . . . . . . . . . 
+. . . 2 . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . 2 . . . . . . . 
+. . . . . . . 2 2 2 . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . 2 . . . . . . . . . . 
+. . . . . 2 . 2 2 . . . . . . . 
+. . . . . . . . 2 . . . . . . . 
+. . . . . . . . 2 . . . . . . . 
+. . . . . . . . . . 2 2 2 . . . 
+2 2 . . 2 2 . . . . 2 2 2 . . . 
+2 2 . . 2 2 . . . . 2 2 2 . . . 
+2 2 . . . . . . . . 2 2 2 . . . 
+. . . . . 2 . . . . 2 2 2 . . . 
+. . . . . 2 . . . . 2 2 2 . . . 
+. . . 2 2 2 . . . 2 2 2 2 . . . 
+. . . 2 2 2 . . . . 2 2 2 . . . 
+. . . . . 2 2 . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0,sprites.builtin.brick,myTiles.tile1,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6],
+            TileScale.Sixteen
+        ))
+game.showLongText("come in asteroid 24370. come in asteroid 24370. get back to our planet (a green line that is yellow dotted) with out touching the green of other planets.  ", DialogLayout.Bottom)
+game.showLongText("remember your training. press A to stop moving and press it again to move again.", DialogLayout.Bottom)
+mySprite.vy = 30
+scene.cameraFollowSprite(mySprite)
+controller.moveSprite(mySprite, 100, 0)
+is_button_A_pressed = true
+info.setScore(500)
+info.setLife(50)
+forever(function () {
+    if (info.score() == -1) {
+        info.changeScoreBy(1)
+    }
+})
+forever(function () {
+    pause(100)
+    info.changeScoreBy(-1)
+})
+forever(function () {
+    if (!(is_button_A_pressed)) {
+        mySprite.vy = 30
+    } else {
+        mySprite.vy = 0
+    }
+})
